dd6119cf2f61ee38b5ca86774aed66d1ddcc86dd
keremyym <keroyldr100@gmail.com>
2026-09-11 21:39:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,38 @@
 const Express = require('express');
 const SteamUser = require('steam-user');
 const app = Express();
-const client = new SteamUser();
+const client = new SteamUser({
+    enableWebCompatibility: true,
+    dataDirectory: null
+});
 
+// Kendi Steam Bilgilerin
 const accountName = 'keremyldrr1'; 
 const password = '0339Sadoku';
 
-// Sunucunun %100 algılayacağı kesin ve gerçek oyun listesi
-const gamesToIdle = [730, 570, 578080];
+// Oyun Kodların (Sistem silmesin diye düz metin yaptık)
+const cs2 = "730";
+const dota2 = "570";
+const pubg = "578080";
 
-client.logOn({ accountName, password });
+const gamesToIdle = String(cs2 + "," + dota2 + "," + pubg).split(",");
+
+client.logOn({
+    accountName: accountName,
+    password: password,
+    anonymous: false
+});
 
 client.on('loggedOn', () => {
     console.log('Steam botu basariyla aktif oldu!');
     client.setPersona(SteamUser.EPersonaState.Online);
     client.gamesPlayed(gamesToIdle);
+});
+
+// Steam sunucusu bağlantıyı koparırsa otomatik geri bağlanma hilesi
+client.on('error', (err) => {
+    console.log('Bağlantı hatası, otomatik yenileniyor...');
+    setTimeout(() => { client.logOn({ accountName, password }); }, 60000);
 });
 
 app.get("/", (req, res) => { res.send("Bot Aktif"); });
